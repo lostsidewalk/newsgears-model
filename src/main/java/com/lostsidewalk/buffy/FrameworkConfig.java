9300b148794f0a7ed29156084afb74db8302333a
elderscrollsjesus <elderscrollsjesus@lostsidewalk.com>
2023-03-15 10:03:38 -0500

    Refactored FrameworkConfig; added display config to framework config

--- src/main/java/com/lostsidewalk/buffy/FrameworkConfig.java
+++ src/main/java/com/lostsidewalk/buffy/FrameworkConfig.java
@@ -1,66 +1,29 @@
 package com.lostsidewalk.buffy;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.util.Optional.ofNullable;
-
 @SuppressWarnings("unused")
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class FrameworkConfig {
 
     public static final String USERNAME = "username";
 
-    private Long userId;
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
+    Long userId;
     //
+    // notifications
     //
-    //
-
     public static final String NOTIFICATION_CONFIG = "notifications";
 
-    private Map<String, String> notifications = new HashMap<>();
+    Map<String, String> notifications = new HashMap<>();
+    //
+    // display
+    //
+    public static final String DISPLAY_CONFIG = "display";
 
-    public Map<String, String> getNotifications() {
-        return notifications;
-    }
-
-    public void setNotifications(Map<String, String> notifications) {
-        this.notifications = notifications;
-    }
-
-    @Override
-    public String toString() {
-        return "Framework Configuration {" +
-                " notifications=" + notifications +
-                "}";
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (!(other instanceof FrameworkConfig otherFrameworkConfig)) {
-            return false;
-        }
-
-        boolean isUsernameEq = ofNullable(this.userId)
-                .map(u -> u.equals(otherFrameworkConfig.getUserId()))
-                .orElse(false);
-        if (!isUsernameEq) {
-            return false;
-        }
-
-        return ofNullable(this.notifications)
-                .map(m -> m.equals(otherFrameworkConfig.getNotifications()))
-                .orElse(false);
-    }
+    Map<String, String> display = new HashMap<>();
 }
